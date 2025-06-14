87f4c80cf4540e9184d06614e34df7c8d60e051f
PallaviGaikwad25 <pgaikwad2587@gmail.com>
2025-06-14 11:42:05 +0530

    Add files via upload

---
+++ assign16_1.py
@@ -0,0 +1,10 @@
+#def studentinfo(n):
+print("enter the student information")
+n=int(input())
+data=list()
+for i in range(1,n):
+    num=(input())
+    data.append(num)
+print(num)
+
+#studentinfo(n)
